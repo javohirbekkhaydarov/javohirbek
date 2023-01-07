60479c059c23408415f59a7125e567cb979a9798
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-01-07 20:12:03 +0500

    add fizzbuzz

--- js/app.js
+++ js/app.js
@@ -21,3 +21,15 @@ function sum(n) {
   return sum;
 }
 console.log(sum(5));
+
+
+// fizz buzz in Javascript
+
+for(let i = 1; i <= 5; i++) {
+  if(!(i % 15)) console.log('FizzBuzz')
+  else if(!(i % 3)) console.log('Fizz')
+  else if (!(i % 5)) console.log('Buzz')
+  else {
+    console.log(i)
+  }
+}
